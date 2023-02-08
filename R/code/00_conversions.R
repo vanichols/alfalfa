94ec8e91896abb48c78441727c4cafe85850a22a
vanichols <virginia.nichols@gmail.com>
2023-02-08 15:59:04 -0600

    working on pesticides

--- R/code/00_conversions.R
+++ R/code/00_conversions.R
@@ -19,3 +19,12 @@ ha_per_ac <- 1/ac_per_ha
 
 btu_per_mj <- conv_unit(1, "J", "BTU") * 1000 * 1000
 mj_per_btu <- 1 / btu_per_mj
+
+
+pints_per_gal <- conv_unit(1, "us_gal", "us_pint")
+gal_per_pint <- 1/pints_per_gal
+
+oz_per_gal <- conv_unit(1, "us_gal", "us_oz")
+gal_per_oz <- 1/oz_per_gal
+
+g_per_gal_water <- 3785.41
