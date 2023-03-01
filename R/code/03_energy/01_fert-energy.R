90ece6a3f256868450e8d4b19a6577e9c91157e6
vanichols <virginia.nichols@gmail.com>
2023-03-01 15:50:25 -0700

    cleaned up fert energy

--- R/code/03_energy/01_fert-energy.R
+++ R/code/03_energy/01_fert-energy.R
@@ -1,42 +1,50 @@
 # calculate energy use
 #created 2/16
+#--3/1 clean up and check
 
 rm(list = ls())
 library(tidyverse)
 source("R/code/00_conversions.R")
+source("R/code/00_funs.R")
 
 
-# assumptions -------------------------------------------------------------
 
-#--assumed fertilizer manufacturing energy, from greet tables
-a_fe <- 
-  read_csv("R/data_refs/ref_fert-energy.csv") %>% 
-  rename(
-    unit_ass = unit,
-    value_ass = value)
+# references --------------------------------------------------------------
 
-a_fe
+#--fertilizer manufacturing energy, from greet tables
+fe <- read_csv("R/data_refs/ref_fert-energy.csv") |> 
+  mutate(mj_kgprod = value,
+         component = desc,
+         desc = cat) |> 
+  select(desc, component, mj_kgprod)
+
+
+
+# production info ---------------------------------------------------------
 
 # how much fertilizer did we apply?
 
 f <- read_csv("R/data_tidy/prod_fertility.csv")
 
-f1 <- 
-  f %>% 
-  #--get names ready to merge
-  select(-cat) %>% 
-  rename(cat = desc) %>% 
-  left_join(a_fe, 
-            by = c("cat")) 
+#--combine with fert energy
 
+f1 <- 
+  f |> 
+  left_join(fe)
+
+
+#--calculate energy used to manufacture each component applied
 f2 <- 
   f1 %>% 
-  mutate(value2 = value * value_ass,
-         unit = "mj/stand") %>%
-  unite(cat, desc, col = "desc", sep = ", ") %>% 
-  mutate(cat = "fertilizer manufacture") %>% 
-  select(production_id, cat, desc, unit, value2) %>% 
-  rename(value = value2)
+  mutate(mj_stand = value * mj_kgprod) |> 
+  #--clean up col names
+  unite(desc, component, col = "desc", sep = ", ") |> 
+  select(production_id, cat, desc, mj_stand) |> 
+  mutate(unit = "mj/stand") |> 
+    rename(value = mj_stand)
+    
+    
+f2
 
 f2 %>% 
   write_csv("R/data_tidy/energy_fert.csv")
